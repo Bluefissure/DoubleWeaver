715f568888eafae8f9f8e3a28ddd57751f610d72
Bluefissure <bluefissure@foxmail.com>
2023-01-15 21:33:40 -0600

    Update

--- DoubleWeaver/Properties/AssemblyInfo.cs
+++ DoubleWeaver/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DoubleWeaver")]
-[assembly: AssemblyCopyright("Copyright © Bluefissure 2021")]
+[assembly: AssemblyCopyright("Copyright © Bluefissure 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.1")]
+[assembly: AssemblyFileVersion("1.0.6.1")]
